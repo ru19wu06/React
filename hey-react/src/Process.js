29b64524aa7c8b9217100d29c9874ff0e5970310
Aaron <43335377+ru19wu06@users.noreply.github.com>
2021-03-16 10:56:55 +0800

    新增戶動元件

--- hey-react/src/Process.js
+++ hey-react/src/Process.js
@@ -1,8 +1,11 @@
 import React,{useState} from 'react';
 
 
-var cos = 50;
-var afet = "%";
+var cos = 30;
+var afet = "";
+
+var sd = 0;
+var fruits = ['','Buy', 'Bitcoin','Now!'];
 const Progress =()=>{
     const [percent,changePercent] = useState("30%");
     console.log(useState("初始值"))
@@ -10,7 +13,9 @@ const Progress =()=>{
         <div>
             <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
             <div className="progress-bar" style={{backgroundColor:"#fe5196",width:percent,height:"100%",borderRadius:"10px"}}></div>
-           </div>
+            
+            </div>
+            <br/>
            {/* <button onClick={()=>{changePercent("70%")}}>轉換百分比</button> */}
            <button onClick={()=>{
                 cos=cos+10;
@@ -18,13 +23,26 @@ const Progress =()=>{
                     cos = 100;
                 }
                 afet = cos+"%";
+                
+                sd++;
+                if(sd>=4){
+                    sd=0;
+                }
+                var text=fruits[sd];
+                document.getElementById('character-poster').innerHTML = text;
 
+                
                changePercent(afet)}
-            }>轉換百分比</button>
+            }>轉換百分比並做一些酷酷的事情</button>
+        <br/>
+        <br/>
+            <div style={{backgroundColor:"rgb(255,215,0)",width:"200px",height:"70px",borderRadius:"5px"}}>
+                <h1 style={{marginLeft:"25%",marginTop:"20%"}} id="character-poster"></h1>
+            </div>
         </div>
     );
 }
 
-
+// document.getElementById('count-area').innerHTML = count;
 
 export default Progress;
